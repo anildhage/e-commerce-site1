deb9c2b6f6bcb12b0f4bef5b71bb6789847d3492
Admin <admin@admins-imac-2.home>
2022-06-15 14:38:59 +1000

    practice only

---
+++ Ecomsite1/app1/admin.py
@@ -0,0 +1,6 @@
+from cmath import phase
+from django.contrib import admin
+from .models import Phone
+
+# Register your models here.
+admin.site.register(Phone)
---
+++ Ecomsite1/app1/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return HttpResponse("Welcome, This is your home page.")
+
+def home(request):
+    return HttpResponse("Welcome, This is your home page.")
